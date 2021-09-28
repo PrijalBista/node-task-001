0673540c38bc92790c49a18f026c120eb14e4ed9
PrijalBista <pri1.zzal@gmail.com>
2021-09-28 16:59:49 +0545

    Add model and migrations for company_category and company

---
+++ database/migrations/1632823785853_companies.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Companies extends BaseSchema {
+  protected tableName = 'companies'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      // table.integer('category_id').unsigned().references('company_categories.id')
+      table.integer('category_id').unsigned().references('id').inTable('company_categories')
+      table.string('title').notNullable()
+      table.string('image')
+      table.text('description')
+      table.boolean('status').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
